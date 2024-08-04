98045e7bd15601f56aa732597b9193d766e5c5b9
GerJaeger <jaeger_42@gmx.de>
2024-08-04 19:33:05 +0200

    Finished the project

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
     plain_text = lower_text(text)
     char_dictionary = get_chars(plain_text)
-    print(char_dictionary)
-    print(char_dictionary.sort())
+    free_dict = remove_symboles(char_dictionary)
+    sorted_dict = sort_dictonary(free_dict)
+    send_report(book_path, num_words, sorted_dict)
 
 def get_num_words(text):
     words = text.split()
@@ -29,7 +29,22 @@ def get_chars(text):
             counter[chars] = 1
     return counter
 
-def sort_dictionary(unsorted_dic):
-    return unsorted_dic.sort()
+def remove_symboles(dicto):
+    free_dicto = {}
+    for key in dicto:
+        if key.isalpha():
+            free_dicto[key] = dicto[key]
+    return free_dicto 
+
+def sort_dictonary(dicto): 
+    new_dicto = {}
+    new_dicto = sorted(dicto.items(), key=lambda item: item[1], reverse=True)
+    return new_dicto
+
+def send_report(path, count, dict_list):
+    print(f"---Begin report of {path} ---\n{count} words found in the document\n")
+    for entry in dict_list:
+        print(f"The '{entry[0]}' character was found {entry[1]} times")
+    print("--- End report ---")  
 
 main()
